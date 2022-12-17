61b8c53904b744f76c5dde3cd4255a5a8978e9ed
lenalog13 <56520937+lenalog13@users.noreply.github.com>
2022-12-17 19:33:33 +0300

    logic

--- ColorSetting/ViewController.swift
+++ ColorSetting/ViewController.swift
@@ -9,18 +9,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorView: UIView!
     
-  
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-        
+        colorView.layer.cornerRadius = 10
+        setColorView()
     }
     
 
+    @IBAction func pulledRedSlider() {
+        setColorView()
+        redLabel.text = String(redSlider.value)
+    }
+    
+    @IBAction func pulledGreenSlider() {
+        setColorView()
+    }
+    
+    @IBAction func pulledBlueSlider() {
+        setColorView()
+    }
     
     
+    private func setColorView() {
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
+                                            green: CGFloat(greenSlider.value),
+                                            blue: CGFloat(blueSlider.value),
+                                            alpha: 1)
+    }
     
 }
 
